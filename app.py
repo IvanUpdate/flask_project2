e83b4be5f38a25244a706114e2ba662789424369
IvanUpdate <mironovvan99@gmail.com>
2020-12-29 02:34:44 +0300

    finished project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from random import sample
 import json
 from data import goals
 from days import days
@@ -8,17 +9,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_main():
-    return "главная"
+    with open("data.json", "r", encoding='utf-8') as f:
+        teachers_list = json.load(f)
+    return render_template('index.html', teachers=sample(teachers_list, 6), goals = goals)
 
 
 @app.route('/all/')
 def render_all():
-    return "здесь будут преподаватели"
+    with open("data.json", "r", encoding='utf-8') as f:
+        teachers_list = json.load(f)
+    return render_template('all.html', teachers=teachers_list, number_of_teachers=len(teachers_list))
 
 
 @app.route('/goals/<goal>/')
 def render_goal(goal):
-    return "здесь будет цель "+goal
+    with open("data.json", "r", encoding='utf-8') as f:
+        teachers_list = json.load(f)
+    teachers_by_goal = []
+    for teacher in teachers_list:
+        if goal in teacher["goals"]:
+            teachers_by_goal.append(teacher)
+    return render_template('goal.html', goal=goals[goal], teachers_by_goal=teachers_by_goal)
 
 
 @app.route('/profiles/<id>/')
@@ -34,22 +45,39 @@ def render_teacher(id):
 
 @app.route('/request/')
 def render_request():
-    return "здесь будет цель заявка на подбор"
+    return render_template('request.html')
 
 
-@app.route('/request_done/')
+@app.route('/request_done/', methods=['POST'])
 def render_request_done():
-    return "заявка на подбор отправлена"
+    goal = request.form.get("goal")
+    time = request.form.get("time")
+    client_name = request.form.get("clientName")
+    client_phone = request.form.get("clientPhone")
+    temp_dict = {"goal": goal, "client_time": time, "client_name": client_name, "client_phone": client_phone}
+    with open("request.json", "a") as r:
+        json.dump(temp_dict, r)
+    return render_template('request_done.html', goal=goal, client_name=client_name, time=time, client_phone=client_phone, goals=goals)
 
 
 @app.route('/booking/<id>/<day>/<time>/')
-def render_booking(id):
-    return "здесь будет форма бронирования "+id
+def render_booking(id, day, time):
+    with open("data.json", "r", encoding='utf-8') as f:
+        teachers_list = json.load(f)
+    return render_template('booking.html', teacher=teachers_list[int(id)], day=day, time=time)
 
 
-@app.route('/booking_done/')
+@app.route('/booking_done/', methods=['POST'])
 def render_booking_done():
-    return "заявка отправлена"
+    client_weekday = request.form.get("clientWeekday")
+    client_time = request.form.get("clientTime")
+    client_teacher = request.form.get("clientTeacher")
+    client_name = request.form.get("clientName")
+    client_phone = request.form.get("clientPhone")
+    temp_dict = {"client_weekday": client_weekday, "client_time": client_time, "client_teacher": client_teacher, "client_name": client_name, "client_phone": client_phone}
+    with open("booking.json", "a") as b:
+        json.dump(temp_dict, b)
+    return render_template('booking_done.html', time=client_time, day=client_weekday, teacher=client_teacher, client_name=client_name, client_phone=client_phone)
 
 
 if __name__ == '__main__':
